49c8982cabb34978f7650ca2fab55f83e3f7d8e7
leopold-wong <trifywang@163.c9m>
2019-01-11 22:21:24 +0800

    add test1

--- src/com/leopold/test/Test1.java
+++ src/com/leopold/test/Test1.java
@@ -1,5 +1,21 @@
 package com.leopold.test;
 
 public class Test1 {
-
+	public void test1() {
+		System.out.println("Test1.test1()");
+		Thread t = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		t.start();
+	}
+	
+	
+	class InnerTest1 {
+		
+	}
 }
